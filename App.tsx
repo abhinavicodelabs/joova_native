1acdfc2abeb241d1ba77e369f6e4efc01ca6ed63
abhinavicodelabs <abhinav@icodelabs.co>
2024-06-13 18:12:42 +0530

    Removed unused code

--- App.tsx
+++ App.tsx
@@ -1,26 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 import SplashScreen from 'react-native-splash-screen';
 import AppNavigator from './src/screens/AppNavigator';
 
